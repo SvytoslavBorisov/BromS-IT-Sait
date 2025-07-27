bd4f9c4fd0f859078a01b44d88f77829c86b6f7b
SvyTo <sv_borisov03@mail.ru>
2025-07-27 14:11:12 +0400

    First commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // игнорировать любые ошибки ESLint при сборке
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
