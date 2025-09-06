840ca8a62cdc887e9a6515733a3b2a3d0b7c7d9b
SvyTo <sv_borisov03@mail.ru>
2025-09-06 14:37:37 +0400

    17

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,22 +2,22 @@ import localFont from "next/font/local";
 
 export const poppins = localFont({
   src: [
-    { path: "/fonts/Poppins-400.woff2", weight: "400", style: "normal" },
-    { path: "/fonts/Poppins-600.woff2", weight: "600", style: "normal" },
-    { path: "/fonts/Poppins-700.woff2", weight: "700", style: "normal" },
+    { path: "../public/fonts/Poppins-400.woff2", weight: "400", style: "normal" },
+    { path: "../public/fonts/Poppins-600.woff2", weight: "600", style: "normal" },
+    { path: "../public/fonts/Poppins-700.woff2", weight: "700", style: "normal" },
   ],
   variable: "--font-poppins",
   display: "swap",
 });
 
 export const geistSans = localFont({
-  src: "/fonts/GeistVF.woff2",
+  src: "../public/fonts/GeistVF.woff2",
   variable: "--font-geist-sans",
   display: "swap",
 });
 
 export const geistMono = localFont({
-  src: "/fonts/GeistMonoVF.woff2",
+  src: "../public/fonts/GeistMonoVF.woff2",
   variable: "--font-geist-mono",
   display: "swap",
 });
