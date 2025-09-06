cb28ac006a6c6dd22598091cbb3a446d6629e83b
SvyTo <sv_borisov03@mail.ru>
2025-09-06 14:43:21 +0400

    17

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -4,8 +4,6 @@ import localFont from "next/font/local";
 export const poppins = localFont({
   src: [
     { path: "../../public/fonts/Poppins-400.woff2", weight: "400", style: "normal" },
-    { path: "../../public/fonts/Poppins-600.woff2", weight: "600", style: "normal" },
-    { path: "../../public/fonts/Poppins-700.woff2", weight: "700", style: "normal" },
   ],
   variable: "--font-poppins",
   display: "swap",
