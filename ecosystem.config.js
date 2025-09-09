a166f650a3140ece7b59e2d383670430877280b0
SvyTo <sv_borisov03@mail.ru>
2025-09-09 12:16:52 +0400

    18

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,18 +5,23 @@ module.exports = {
       cwd: ".",
       script: "node_modules/.bin/next",
       args: "start -p 3000",
-      env_file: ".env",
-      env: {
-        NODE_ENV: "production",
-        PORT: "3000",
-        NODE_OPTIONS: "--max-old-space-size=512 --initial-old-space-size=256"
-      },
-      instances: 1,      // можно увеличить/перейти в cluster после
-      autorestart: true,
       exec_mode: "fork",
       instances: 1,
       watch: false,
+      autorestart: true,
       max_restarts: 10,
+
+      // <— вот здесь передаем V8-флаги
+      interpreter: "/usr/bin/node", // можно не указывать, если и так /usr/bin/node
+      interpreter_args: "--initial-old-space-size=256 --max-old-space-size=512",
+
+      env_file: ".env",
+      env: {
+        NODE_ENV: "production",
+        PORT: "3000"
+        // НЕ дублировать NODE_OPTIONS
+      },
+
       out_file: "logs/out.log",
       error_file: "logs/err.log",
       time: true
